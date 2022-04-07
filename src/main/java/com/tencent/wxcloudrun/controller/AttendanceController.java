981fe635873c64a650b4932770f0b35b98719ed5
iamcorey <390583019@qq.com>
2022-04-07 19:55:56 +0800

    modify attendance controller

--- src/main/java/com/tencent/wxcloudrun/controller/AttendanceController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/AttendanceController.java
@@ -1,5 +1,9 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
@@ -23,11 +27,11 @@ import java.util.Optional;
 public class AttendanceController {
 
   final CounterService counterService;
-  final Logger logger;
+  final Logger log;
 
   public AttendanceController(@Autowired CounterService counterService) {
     this.counterService = counterService;
-    this.logger = LoggerFactory.getLogger(AttendanceController.class);
+    this.log = LoggerFactory.getLogger(AttendanceController.class);
   }
 
 
@@ -38,11 +42,35 @@ public class AttendanceController {
    */
   @PostMapping(value = "/attendanceService/submit")
   ApiResponse submitAttendance(@RequestBody String req) {
-    logger.info("Receive submit attendance request: {}", req);
+    log.info("Receive submit attendance request: {}", req);
+
+    Gson gson = new GsonBuilder()
+            // 禁止unicode转义
+            .disableHtmlEscaping()
+            .create();
+
+    try {
+      JsonObject reqJs = gson.fromJson(req, JsonObject.class);
+
+      // employee name
+      String name = reqJs.get("name").getAsString();
+      // employee id
+      String id = reqJs.get("id").getAsString();
+      // 迟到日期
+      String lateDate = reqJs.get("lateDateList").getAsString();
+      // 早退日期
+      String earlyDate = reqJs.get("earlyDateList").getAsString();
+
+      
 
 
+      return ApiResponse.ok(req);
 
-    return ApiResponse.ok(req);
+    } catch (JsonSyntaxException jsonSyntaxException) {
+      log.warn(jsonSyntaxException.getMessage());
+      return ApiResponse.error("JSON解析失败 / JSON parse fail");
+    }
+
   }
   
 }
